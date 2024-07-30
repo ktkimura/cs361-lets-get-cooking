65ca6f0bc7cdc93411522fd41e76a162879e3dd1
Katie Kimura <kimuraka@oregonstate.edu>
2024-07-30 08:50:31 -1000

    prepping for microservice A integration

--- lets-get-cooking/frontend/src/pages/ViewRecipePage.js
+++ lets-get-cooking/frontend/src/pages/ViewRecipePage.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+const fs = require('fs');
 
 function ViewRecipePage(){
-
+    const fs = require('fs')
     const { id } = useParams();
-    //const [recipe, setRecipe]                               = useState(null);
     const [name, setName]                                   = useState('');
     const [ingredients, setIngredients]                     = useState('');
     const [instructions, setInstructions]                   = useState('');
@@ -19,13 +19,25 @@ function ViewRecipePage(){
     *   Source URL: https://medium.com/weekly-webtips/use-react-with-json-server-and-create-simple-crud-app-b2bf58cd4558 
     */
     useEffect(() => {
+        //const rawData = fs.readFileSync('./data/ingredientCompare.json');
+        let allIngredientsArr;
+
+        fetch("http://localhost:8000/ingredients")
+            .then(res => res.json())
+            .then(result => {
+                allIngredientsArr = JSON.stringify(result);
+        })
+        
+
         fetch(`http://localhost:8000/recipes/${id}`)    //get specific recipe data to populate fields
             .then(res => res.json())
             .then(data => {
-                //setRecipe(data);
                 setName(data.name);
                 setIngredients(data.ingredients.join(', '));
                 setInstructions(data.instructions);
+
+                //let recipeIngredientsArr = data.ingredients;
+                fs.writeFileSync('./data/ingredientCompare.json', allIngredientsArr)
             })
     }, [id]);
 
